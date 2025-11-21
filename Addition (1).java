a07aef89186f720f5b5deabb3d3d32334fa60a1b
nidhipawar12345 <srinidhipawar10@gmail.com>
2025-11-21 10:37:44 +0530

    Add files via upload

---
+++ Addition (1).java
@@ -0,0 +1,13 @@
+public class Addition {
+    public static void main(String[] args) {
+        // Declare two numbers
+        int num1 = 10;
+        int num2 = 20;
+
+        // Add the numbers
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
